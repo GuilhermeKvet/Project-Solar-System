d7ffec24a9016a0235598f2876a0ac204dab0b3b
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-05-09 20:56:22 -0300

    Estilizando a pagina

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,13 +6,14 @@ import MissionCard from './MissionCard';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missionContainer">
         <Title headline="Missões" />
         {missions.map((mission) => (
           <MissionCard
             key={ mission.name }
             name={ mission.name }
             year={ mission.year }
+            country={ mission.country }
             destination={ mission.destination }
           />
         ))}
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -6,7 +6,7 @@ import planets from '../data/planets';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" className="solarSystem">
         <Title headline="Planetas" />
         {planets.map((planet) => (
           <PlanetCard
